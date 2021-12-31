247f0d8ad36d754043065f35f14313859aee84fe
Sangeeta13Chauhan <53479343+Sangeeta13Chauhan@users.noreply.github.com>
2021-12-31 10:34:40 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,60 @@
+onEvent("english", "click", function( ) {
+  setScreen("englishscreen");
+});
+onEvent("444", "click", function( ) {
+  setScreen("write");
+});
+onEvent("555", "click", function( ) {
+  setScreen("wronge");
+});
+onEvent("wronge", "click", function( ) {
+  setScreen("mainscreen");
+});
+onEvent("sience", "click", function( ) {
+  setScreen("scinc");
+});
+onEvent("button3e", "click", function( ) {
+  setScreen("1234");
+});
+onEvent("button23", "click", function( ) {
+  setScreen("123451");
+});
+onEvent("65", "click", function( ) {
+  setScreen("mainscreen");
+});
+onEvent("234rr", "click", function( ) {
+  setScreen("mainscreen");
+});
+onEvent("writett", "click", function( ) {
+  setScreen("mainscreen");
+});
+onEvent("scince", "click", function( ) {
+  setScreen("mathhhhhhhh");
+});
+onEvent("button--", "click", function( ) {
+  setScreen("noo");
+});
+onEvent("button=-", "click", function( ) {
+  setScreen("mainscreen");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screenbby");
+});
+onEvent("butterf", "click", function( ) {
+  setScreen("mainscreen");
+});
+onEvent("ssc", "click", function( ) {
+  setScreen("scinces343");
+});
+onEvent("buttun453", "click", function( ) {
+  setScreen("wrong6`");
+});
+onEvent("2345bb", "click", function( ) {
+  setScreen("last");
+});
+onEvent("lastbbbbb", "click", function( ) {
+  setScreen("mainscreen");
+});
+onEvent("lastme", "click", function( ) {
+  setScreen("mainscreen");
+});
